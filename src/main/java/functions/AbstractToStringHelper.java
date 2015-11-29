2ffaa56e2b9455b2c68ec3378fba5dad9603120e
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-11-29 10:39:01 -0600

    Remove unnecessary parameterization of type
    The formatObject method formerly had a single object argument of generic type T (declared with the signature "<T>"). However, since T extends Object, its erasure is simply Object. More formally, "the erasure of a type variable ... is the erasure of its leftmost bound" (JLS §4.6). Therefore, the type variable in this context is redundant, as the least generic operation required in the method is a cast to Object[].

--- src/main/java/functions/AbstractToStringHelper.java
+++ src/main/java/functions/AbstractToStringHelper.java
@@ -8,46 +8,46 @@ import java.util.stream.Stream;
 
 /**
  * A mutable {@code String} representation of an arbitrary object.
- * 
+ *
  * <p> This class imposes a single {@code abstract} method, {@link #toString()}.
  * The general contract of this method is to return the data represented by the
  * object with an arbitrary formatting sequence.
- * 
+ *
  * @author Oliver Abdulrahim
  */
 abstract class AbstractToStringHelper {
-    
+
     /**
      * Maps {@code String} tags to {@code Object} properties. The keys stored in
-     * this {@code Map} are formatted with respect to their properties as 
+     * this {@code Map} are formatted with respect to their properties as
      * specified in the {@link #toString()} method for this class.
      */
     private final Map<String, Object> values;
-    
+
     /**
-     * The name of this representation as specified during construction. 
+     * The name of this representation as specified during construction.
      * default, this is the name of the {@code Object} class.
      */
     private final String name;
 
     /**
-     * Constructs an {@code AbstractToStringHelper} with an arbitrary object as 
+     * Constructs an {@code AbstractToStringHelper} with an arbitrary object as
      * the target.
      */
     AbstractToStringHelper() {
         this("Object");
     }
-    
+
     /**
      * Constructs an {@code AbstractToStringHelper} with the given object as the
      * target.
-     * 
+     *
      * @param target The class that this helper represents.
      */
     AbstractToStringHelper(Class<?> target) {
         this(formatClass(target));
     }
-    
+
     /**
      * Constructs an {@code AbstractToStringHelper} with the given name and no
      * target.
@@ -56,17 +56,62 @@ abstract class AbstractToStringHelper {
         this.name = name;
         this.values = new LinkedHashMap<>();
     }
-    
+
+// Mapping operations
+
     /**
-     * Specifies formatting for the name of a given class, returning a 
+     * Returns the The name of this representation as specified during
+     * construction.
+     *
+     * @return The name of this representation.
+     */
+    String getName() {
+        return name;
+    }
+
+    /**
+     * Associates the given property with the given tag within this
+     * representation.
+     *
+     * @param tag The name to associate with the given property.
+     * @param property The property to add with the given name.
+     * @return This object (after the add operation is complete), allowing for
+     *         chaining of operations.
+     */
+    AbstractToStringHelper add(String tag, Object property) {
+        values.put(tag, property);
+        return this;
+    }
+
+    /**
+     * Adds all the entries in the given {@code Map} into this object
+     */
+    void addAll(Map<String, Object> values) {
+        this.values.putAll(values);
+    }
+
+    /**
+     * Returns an set view of the entries of tags and properties contained
+     * within this object.
+     *
+     * @return An set containing the tag-property entries of this object.
+     */
+    Set<Map.Entry<String, Object>> entries() {
+        return values.entrySet();
+    }
+
+// Formatting operations
+
+    /**
+     * Specifies formatting for the name of a given class, returning a
      * {@code String} containing the simple name of the class.
-     * 
+     *
      * <p> If the given class is anonymous, this method returns a {@code String}
      * with the characters {@code "$Anonymous"} prepended with the name of the
      * declaring class with a maximum depth of one.
-     * 
+     *
      * @param c The class to convert to a {@code String}.
-     * @return A {@code String} formatted to contain the name of the given 
+     * @return A {@code String} formatted to contain the name of the given
      *         class.
      */
     private static String formatClass(Class<?> c) {
@@ -84,57 +129,12 @@ abstract class AbstractToStringHelper {
     }
 
     /**
-     * Returns the The name of this representation as specified during 
-     * construction.
-     * 
-     * @return The name of this representation.
-     */
-    String getName() {
-        return name;
-    }
-    
-// Mapping operations    
-    
-    /**
-     * Associates the given property with the given tag within this 
-     * representation.
-     * 
-     * @param tag The name to associate with the given property.
-     * @param property The property to add with the given name.
-     * @return This object (after the add operation is complete), allowing for
-     *         chaining of operations.
-     */
-    AbstractToStringHelper add(String tag, Object property) {
-        values.put(tag, property);
-        return this;
-    }
-    
-    /**
-     * Adds all the entries in the given {@code Map} into this object
-     */
-    void addAll(Map<String, Object> values) {
-        this.values.putAll(values);
-    }
-    
-    /**
-     * Returns an set view of the entries of tags and properties contained
-     * within this object.
-     * 
-     * @return An set containing the tag-property entries of this object.
-     */
-    Set<Map.Entry<String, Object>> entries() {
-        return values.entrySet();
-    }
-    
-// Formatting operations    
-    
-    /**
      * Returns a {@code String} representation of the property, or value, mapped
-     * to the given tag, or key. Wraps the given value if it is an array or 
+     * to the given tag, or key. Wraps the given value if it is an array or
      * {@code null}. See the {@link #formatObject(java.lang.Object)} method for
-     * a more formal description for the {@code String}s returned by this 
+     * a more formal description for the {@code String}s returned by this
      * method.
-     * 
+     *
      * @param tag The key whose associated value to return.
      * @return A formatted property mapped to the given tag.
      * @see #formatObject(java.lang.Object)
@@ -143,34 +143,32 @@ abstract class AbstractToStringHelper {
         Object value = values.get(tag);
         return formatObject(value);
     }
-    
+
     /**
      * Returns a {@code String} representation of the given object, formatting
-     * as necessary. 
-     * 
-     * <p> If the given object is an array, then this method returns a 
+     * as necessary.
+     *
+     * <p> If the given object is an array, then this method returns a
      * comma-separated view of the elements contained within it. If the given
-     * object is {@code null}, then this method returns a {@code String} with 
+     * object is {@code null}, then this method returns a {@code String} with
      * the characters {@code "null"}. Otherwise, returns the value returned by
      * the objects {@code toString} method.
-     * 
-     * @param <T> The type of the given object.
+     *
      * @param obj The object to convert to a {@code String}.
      * @return A {@code String} representation of the given object.
      */
-    @SuppressWarnings("unchecked")
-    private <T> String formatObject(T obj) {
+    private String formatObject(Object obj) {
         String objToString = "";
         if (obj == null) {
             objToString = "null";
         }
-        else if (obj == this) { // Nice try, pal
+        else if (obj == this) {
             objToString = "(this ToStringHelper)";
         }
         // Writing this broke my heart... why, primitive types, why?!
         else if (obj.getClass().isArray()) {
             if (obj instanceof Object[]) {
-                objToString = wrapArray((T[]) obj);
+                objToString = wrapArray((Object[]) obj);
             }
             else if (obj instanceof boolean[]) {
                 objToString = Arrays.toString((boolean[]) obj);
@@ -202,12 +200,12 @@ abstract class AbstractToStringHelper {
         }
         return objToString;
     }
-    
+
     /**
-     * Wraps the given array into a {@code String}. If the array is 
+     * Wraps the given array into a {@code String}. If the array is
      * multidimensional, then the returned {@code String} will contain a
      * representation of the elements in the array.
-     * 
+     *
      * @param <T> The type of the elements in the array.
      * @param array The array to wrap with a {@code String}.
      * @return A {@code String} representation of the given array.
@@ -219,19 +217,19 @@ abstract class AbstractToStringHelper {
                 ? Arrays.deepToString(array)
                 : Arrays.toString(array);
     }
-    
-// Abstract methods    
-    
+
+// Abstract methods
+
     /**
      * Constructs and returns a {@code String} representation of the object or
-     * values wrapped by this one. 
-     * 
+     * values wrapped by this one.
+     *
      * <p> The general contract of this method is to return the data represented
      * by the object with an arbitrary formatting sequence.
-     * 
+     *
      * @return A {@code String} representation of the object.
      */
     @Override
     public abstract String toString();
-    
+
 }
