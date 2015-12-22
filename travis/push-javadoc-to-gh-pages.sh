af11eb4efaa065bcaf35eb6b50110467d4cf6be8
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-12-22 09:26:00 -0600

    Update Javadoc auto-push script

--- travis/push-javadoc-to-gh-pages.sh
+++ travis/push-javadoc-to-gh-pages.sh
@@ -1,20 +1,20 @@
-# From http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+#!/usr/bin/env bash
 
-if [[ "$TRAVIS_REPO_SLUG" == "OliverAbdulrahim/ToStringHelper" && "$TRAVIS_JDK_VERSION" == "oraclejdk8" && "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "build" ]]; then
- 
+if [[ "$TRAVIS_REPO_SLUG" == "OliverAbdulrahim/ToStringHelper" ]]; then
+
   echo -e "Publishing Javadoc...\n"
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${TOKEN}@github.com/OliverAbdulrahim/ToStringHelper gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${TOKEN}@github.com/OliverAbdulrahim/ToStringHelper gh-pages > /dev/null 2>&1
 
   cd gh-pages
   git clean -d -x -f
   cp -Rf $HOME/build/OliverAbdulrahim/ToStringHelper/target/site/apidocs/. $PWD
   git add -f .
   git commit -m "Lastest Javadoc on successful Travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages > /dev/null 2>&1
 
   echo -e "Published Javadoc to gh-pages.\n"
 
